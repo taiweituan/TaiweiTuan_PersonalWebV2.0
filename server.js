6b10f6ffff7e9c19f4f2ae9c8e05a6aa23b3092b
Taiwei Tuan <taiweituan@Taiweis-MacBook-Pro.local>
2016-04-21 17:08:33 -0500

    minor changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var app = express();
 var PORT = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function(){
 	console.log('Listening to port ' +PORT);
 });
@@ -12,4 +15,9 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 // load the single view file (angular will handle the page changes on the front-end)
 app.get('*', function(req, res) {
     res.sendfile('./public/index.html'); 
+});
+
+app.post('/sendEmail', function(req, res) {
+	console.log('send Email');
+    res.status(200);	// return success 
 });
