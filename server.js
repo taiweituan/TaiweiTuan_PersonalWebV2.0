337efdda0dd33be9280b89bf0ce7caab36ef9ad4
Taiwei Tuan <taiweituan@Taiweis-MacBook-Pro.local>
2016-04-26 17:19:33 -0500

    added ability to send mail. Contact page completed

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
-var gmail = process.env.GMAIL_USERNAME || 'gmail';
-console.log(gmail);
+var gmailUsername = process.env.GMAIL_USERNAME || 'gmail';
+var gmailPassword = process.env.GMAIL_PASSWORD || 'password';
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
-
-app.listen(PORT, function(){
-	console.log('Listening to port ' +PORT);
+app.listen(PORT, function() {
+	console.log('Listening to port ' + PORT);
 });
 
 // redirect CSS bootstrap
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 // load the single view file (angular will handle the page changes on the front-end)
 app.get('*', function(req, res) {
-    res.sendfile('./public/index.html'); 
+	res.sendfile('./public/index.html');
 });
 
 app.post('/sendEmail', function(req, res) {
-	console.log('send Email');
-    res.status(200);	// return success 
+	console.log(req.body);
+	var mailOptions = {
+		from: '"Node Reply 👥" <foo@blurdybloop.com>', // sender address
+		to: 'super496@hotmail.com', // list of receivers
+		subject: req.body.title, // Subject line
+		text: req.body.message // plaintext body
+		// html: '<b>Hello world 🐴</b>' // html body
+	};
+	
+	// need to encrypt it somehow, pls send halp
+	var transporter = nodemailer.createTransport(
+		'smtps://'+gmailUsername+'%40gmail.com:'+gmailPassword+'@smtp.gmail.com'
+	);
+	
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('Message sent: ' + info.response);
+	});
+
+	// return sent message 
+	res.json(mailOptions); // return success 
 });
