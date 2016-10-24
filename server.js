26d6184d5078682fd161cab92f5da3276bcfe042
Taiwei Tuan <taiweituan@gmail.com>
2016-10-24 16:05:16 -0500

    Fixed the email issue

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var bodyParser = require("body-parser");
 var app = express();
-var PORT = process.env.PORT || 8080;
-var gmailUsername = process.env.GMAIL_USERNAME || 'test';
-var gmailPassword = process.env.GMAIL_PASSWORD || 'test@';
+var PORT = process.env.PORT || 8081;
+var emailUsername = process.env.EMAIL_USERNAME || 'test';
+var emailPassword = process.env.EMAIL_PASSWORD || 'tester';
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log('Listening to port ' + PORT);
 });
 
@@ -21,32 +21,43 @@ app.listen(PORT, function() {
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 // load the single view file (angular will handle the page changes on the front-end)
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
 	res.sendfile('./public/index.html');
 });
 
-app.post('/sendEmail', function(req, res) {
+
+app.post('/sendEmail', function (req, res) {
 	console.log(req.body);
+
+	// need to encrypt it somehow, pls send halp
+	var transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true, // use SSL
+		auth: {
+			user: emailUsername+'@gmail.com',
+			pass: emailPassword
+		}
+	});
+
 	var mailOptions = {
-		from: '"Node Reply 👥" <foo@blurdybloop.com>', // sender address
-		to: 'super496@hotmail.com', // list of receivers
+		from: '"NodeMailer" <nodemailer@no-reply.com>', // sender address
+		to: 'super496@hotmail.com, taiweituan@gmail.com', // list of receivers
 		subject: req.body.title, // Subject line
 		text: req.body.message // plaintext body
-		// html: '<b>Hello world 🐴</b>' // html body
 	};
-	
-	// need to encrypt it somehow, pls send halp
-	var transporter = nodemailer.createTransport(
-		'smtps://'+gmailUsername+'%40gmail.com:'+gmailPassword+'@smtp.gmail.com'
-	);
-	
+
 	// send mail with defined transport object
-	transporter.sendMail(mailOptions, function(error, info) {
+	transporter.sendMail(mailOptions, function (error, info) {
 		if (error) {
+			console.log("There was an error");
 			return console.log(error);
 		}
 		console.log('Message sent: ' + info.response);
 	});
+
+	// Shut down the connection pool, no more messages. Comment this line out to continue sending emails.
+	transporter.close();
 
 	// return sent message 
 	res.json(mailOptions); // return success 
